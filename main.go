441dae79a9983a0d10ede9db420de10a5ec47ddd
WarLlama <jgeiman12@yahoo.com>
2020-02-20 08:41:13 -0500

    Break into function for normalizing and applying square gradient

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"github.com/aquilax/go-perlin"
@@ -7,56 +8,120 @@ import (
 	"math/rand"
 )
 
+type Grid [][]float64
 const (
-	width = 10
-	height = 10
-	character = "\u2588"
+	width = 100
+	height = 100
+	centerX = 85
+	centerY = 70
+	character = "\u2588\u2588"
 )
 func main() {
-	p := perlin.NewPerlin(2, 2, 10, rand.Int63())
-	maxRadius := math.Hypot(width, height)
-	for x := 0.0; x < width; x+=0.1 {
-		for y := 0.0; y < height; y+=0.1 {
-			centerY := 0.5 * height
-			centerX := 0.5 * width
-			dx := x-centerX
-			dy := y-centerY
-			radius := math.Hypot(dx, dy)
-			max := 1 - radius/(2 * maxRadius)
-			min := 0.4 - radius/(2 *maxRadius)
-			PrintBlock(clamp(p.Noise2D(x, y), max, min))
-		}
-		fmt.Print("\n")
-    }
+    grid := Normalize(GenerateNoise())
+    grid = Normalize(ApplyGradient(centerX, centerY, grid))
+    grid = Normalize(ApplyGradient(20, 30, grid))
+    Print(grid)
+
 }
 
-func clamp(v, max, min float64) float64 {
-	if v > max {
-		return max
+func GenerateNoise() *Grid {
+	multiplier := 0.1
+	p := perlin.NewPerlin(2, 2, 10, rand.Int63())
+	var grid Grid = make([][]float64, 0)
+
+	for x := 0; x < width; x++ {
+		row := make([]float64, 0)
+		fx := float64(x)
+		for y := 0; y < height; y++ {
+			fy := float64(y)
+			z := p.Noise2D(fx * multiplier, fy * multiplier)
+			row = append(row, z)
+		}
+		grid = append(grid, row)
+    }
+    return &grid
+}
+
+func Normalize(grid *Grid) *Grid {
+	max := 0.0
+	min := 0.0
+    for _, row := range *grid {
+		for _, cell := range row {
+			min = math.Min(min, cell)
+			max = math.Max(max, cell)
+		}
 	}
-	if v < min {
-		return min
+	rangeDist := max - min
+	var newGrid Grid = make([][]float64, 0)
+    for _, row := range *grid {
+		newRow := make([]float64, 0)
+    	for _, cell := range row {
+    		newRow = append(newRow, (cell+math.Abs(min))/rangeDist)
+		}
+		newGrid = append(newGrid, newRow)
 	}
-	return v
+	return &newGrid
+}
+
+func ApplyGradient(centerX, centerY float64, grid *Grid) *Grid {
+	var newGrid Grid = make([][]float64, 0)
+	lenX := float64(len(*grid)-1)
+    for x, row := range *grid {
+		fx := float64(x)
+		lenY := float64(len(row)-1)
+		newRow := make([]float64, 0)
+    	for y, cell := range row {
+			fy := float64(y)
+			xgradient := 0.0
+			if fx < centerX {
+				xgradient = 1 - fx/centerX
+			} else {
+				xgradient = (fx-centerX)/(lenX-centerX)
+			}
+			ygradient := 0.0
+			if fy < centerY {
+				ygradient = 1 - fy/centerX
+			} else {
+				ygradient = (fy-centerY)/(lenY-centerY)
+			}
+			gradient := 1 - math.Max(xgradient, ygradient)
+    		newRow = append(newRow, cell * gradient)
+		}
+		newGrid = append(newGrid, newRow)
+	}
+	return &newGrid
+}
+
+func Print(grid *Grid) {
+	for _, row := range *grid {
+    	for _, cell := range row {
+			PrintBlock(cell)
+		}
+		fmt.Print("\n")
+	}
+}
+
+func PrintGray(v float64) {
+	fmt.Print(aurora.Gray(uint8(v * 24), character))
 }
 
 func PrintBlock(v float64) {
-	if v > 0.8 {
+	if v > 0.7 {
 		fmt.Print(aurora.Gray(22, character))
 		return
 	}
-	if v > 0.6 {
+	if v > 0.3 {
 		fmt.Print(aurora.Green(character))
 		return
 	}
-	if v > 0.3 {
+	if v > 0.2 {
 		fmt.Print(aurora.Yellow(character))
 		return
 	}
-	if v > 0.2 {
+	if v > 0.1 {
 		fmt.Print(aurora.BrightBlue(character))
 		return
 	}
-		fmt.Print(aurora.Blue(character))
-		return
+	fmt.Print(aurora.Blue(character))
+	return
 }
