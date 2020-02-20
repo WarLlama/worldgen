d5fb2f39f81cd7facae2eb0b22f7ce57000aa6fc
Jacob Geiman <jgeiman12@yahoo.com>
2020-02-20 11:30:20 -0500

    Add normal distribution

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/aquilax/go-perlin v0.0.0-20191229124216-0af9ce917c28
 	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
+	gonum.org/v1/gonum v0.6.2
 )
--- main.go
+++ main.go
@@ -4,28 +4,46 @@ import (
 	"fmt"
 	"github.com/aquilax/go-perlin"
 	"github.com/logrusorgru/aurora"
+	"gonum.org/v1/gonum/stat/distuv"
 	"math"
 	"math/rand"
+	"time"
 )
 
 type Grid [][]float64
+
 const (
-	width = 100
-	height = 100
-	centerX = 85
-	centerY = 70
+	width     = 100
+	height    = 100
+	centerX   = 0.3 * width
+	centerY   = 0.4 * height
 	character = "\u2588\u2588"
 )
+
 func main() {
-    grid := Normalize(GenerateNoise())
-    grid = Normalize(ApplyGradient(centerX, centerY, grid))
-    grid = Normalize(ApplyGradient(20, 30, grid))
-    Print(grid)
+	grid := Normalize(GenerateNoise())
+	grid = Normalize(NormalDistribution(centerX, centerY, grid))
+	grid = Normalize(SquareGradient(centerX, centerY, grid))
+	Print(grid)
 
 }
 
+func NewGrid(width, height uint64) *Grid {
+	var newGrid Grid = make([][]float64, 0)
+	for x := uint64(0); x < width; x++ {
+		row := make([]float64, 0)
+		for y := uint64(0); y < width; y++ {
+			row = append(row, 1)
+		}
+		newGrid = append(newGrid, row)
+	}
+	return &newGrid
+}
+
 func GenerateNoise() *Grid {
-	multiplier := 0.1
+	rand.Seed(time.Now().UnixNano())
+	_, multiplier := math.Modf(rand.Float64())
+	multiplier = 0.1 * multiplier + 0.1
 	p := perlin.NewPerlin(2, 2, 10, rand.Int63())
 	var grid Grid = make([][]float64, 0)
 
@@ -34,18 +52,18 @@ func GenerateNoise() *Grid {
 		fx := float64(x)
 		for y := 0; y < height; y++ {
 			fy := float64(y)
-			z := p.Noise2D(fx * multiplier, fy * multiplier)
+			z := p.Noise2D(fx*multiplier, fy*multiplier)
 			row = append(row, z)
 		}
 		grid = append(grid, row)
-    }
-    return &grid
+	}
+	return &grid
 }
 
 func Normalize(grid *Grid) *Grid {
 	max := 0.0
 	min := 0.0
-    for _, row := range *grid {
+	for _, row := range *grid {
 		for _, cell := range row {
 			min = math.Min(min, cell)
 			max = math.Max(max, cell)
@@ -53,39 +71,66 @@ func Normalize(grid *Grid) *Grid {
 	}
 	rangeDist := max - min
 	var newGrid Grid = make([][]float64, 0)
-    for _, row := range *grid {
+	for _, row := range *grid {
 		newRow := make([]float64, 0)
-    	for _, cell := range row {
-    		newRow = append(newRow, (cell+math.Abs(min))/rangeDist)
+		for _, cell := range row {
+			newRow = append(newRow, (cell+math.Abs(min))/rangeDist)
 		}
 		newGrid = append(newGrid, newRow)
 	}
 	return &newGrid
 }
 
-func ApplyGradient(centerX, centerY float64, grid *Grid) *Grid {
+func SquareGradient(centerX, centerY float64, grid *Grid) *Grid {
 	var newGrid Grid = make([][]float64, 0)
-	lenX := float64(len(*grid)-1)
-    for x, row := range *grid {
+	lenX := float64(len(*grid) - 1)
+	for x, row := range *grid {
 		fx := float64(x)
-		lenY := float64(len(row)-1)
+		lenY := float64(len(row) - 1)
 		newRow := make([]float64, 0)
-    	for y, cell := range row {
+		for y, cell := range row {
 			fy := float64(y)
 			xgradient := 0.0
 			if fx < centerX {
 				xgradient = 1 - fx/centerX
 			} else {
-				xgradient = (fx-centerX)/(lenX-centerX)
+				xgradient = (fx - centerX) / (lenX - centerX)
 			}
 			ygradient := 0.0
 			if fy < centerY {
 				ygradient = 1 - fy/centerX
 			} else {
-				ygradient = (fy-centerY)/(lenY-centerY)
+				ygradient = (fy - centerY) / (lenY - centerY)
 			}
 			gradient := 1 - math.Max(xgradient, ygradient)
-    		newRow = append(newRow, cell * gradient)
+			newRow = append(newRow, cell*gradient)
+		}
+		newGrid = append(newGrid, newRow)
+	}
+	return &newGrid
+}
+
+func NormalDistribution(centerX, centerY float64, grid *Grid) *Grid {
+	ndX := distuv.Normal{
+		Mu:    centerX,
+		Sigma: float64(len(*grid))/3,
+	}
+
+	ndY := distuv.Normal{
+		Mu:    centerY,
+		Sigma: float64(len((*grid)[0]))/3,
+	}
+	var newGrid Grid = make([][]float64, 0)
+	for x, row := range *grid {
+		fx := float64(x)
+		newRow := make([]float64, 0)
+		for y, cell := range row {
+
+			fy := float64(y)
+			xgradient := ndX.Prob(fx)
+			ygradient := ndY.Prob(fy)
+			gradient := math.Min(xgradient, ygradient)
+			newRow = append(newRow, cell*gradient)
 		}
 		newGrid = append(newGrid, newRow)
 	}
@@ -94,7 +139,7 @@ func ApplyGradient(centerX, centerY float64, grid *Grid) *Grid {
 
 func Print(grid *Grid) {
 	for _, row := range *grid {
-    	for _, cell := range row {
+		for _, cell := range row {
 			PrintBlock(cell)
 		}
 		fmt.Print("\n")
@@ -102,7 +147,7 @@ func Print(grid *Grid) {
 }
 
 func PrintGray(v float64) {
-	fmt.Print(aurora.Gray(uint8(v * 24), character))
+	fmt.Print(aurora.Gray(uint8(v*24), character))
 }
 
 func PrintBlock(v float64) {
